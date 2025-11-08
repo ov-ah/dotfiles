ce5665a2e98d35bd91e0ea8b780bbfd2aedd003d
ovah <alloris.666@gmail.com>
2025-11-07 16:09:44 -0800

    added dwm and st configs

--- sym.sh
+++ sym.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-ln -s "$HOME/dotfiles/.emacs" "$HOME/.emacs"
+ln -sf "$HOME/dotfiles/.emacs" "$HOME/.emacs"
+ln -sf "$HOME/dotfiles/dwm-config.h" "$HOME/dwm/config.h"
+ln -sf "$HOME/dotfiles/st-config.h" "$HOME/st/config.h"
