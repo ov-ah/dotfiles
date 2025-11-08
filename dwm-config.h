2601c50bd0bf1f4429c7e9c898bad31da72f04ee
ovah <alloris.666@gmail.com>
2025-11-07 16:38:24 -0800

    dwm colors updated

--- dwm-config.h
+++ dwm-config.h
@@ -7,10 +7,10 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "terminus:size=10" };
 static const char dmenufont[]       = "terminus:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
+static const char col_gray1[]       = "#101010";
+static const char col_gray2[]       = "#181818";
+static const char col_gray3[]       = "#282828";
+static const char col_gray4[]       = "#383838";
 static const char col_cyan[]        = "#005577";
 static const char col_yellow[]      = "#ffdd33";
 static const char *colors[][3]      = {
