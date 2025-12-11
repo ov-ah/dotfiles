cd2a052ece373cbd3c47f780fbc479aad8004dc5
ovah <admin@ovah.dev>
2025-12-11 01:51:47 -0800

    init commit

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+link()
+{
+	if (($# != 2)); then
+		ln -sfn $HOME/dotfiles/$1 $HOME/$1
+	else
+		ln -sfn $HOME/dotfiles/$1 $HOME/$2
+	fi
+}
+
+link .vimrc
+link .bashrc
+link .gitconfig
